162bb08d3d231d0d3b3b9b1a248239c5f1b6cb53
ibladmin <alejandropan@gmail.com>
2019-06-12 01:56:00 -0400

    final glm

--- behavior_analysis/glm.py
+++ behavior_analysis/glm.py
@@ -34,10 +34,14 @@ import time
 import statsmodels.genmod.bayes_mixed_glm as  bayes
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+def glm_reward(psy_df,  mixed_effects=False, sex =  True):
+from sklearn.model_selection import KFold
 
-def glm_reward(psy_df,  mixed_effects=True):
-    
-    ##calculate useful variables
+ 
+def  glm_logit(psy_df):
+
+   ##calculate useful variables
     
     #Calculate signed contrast
     if not 'signed_contrasts' in psy_df:
@@ -45,19 +49,6 @@ def glm_reward(psy_df,  mixed_effects=True):
         psy_df.loc[:,'contrastLeft']  = psy_df['contrastLeft'].fillna(0)
         psy_df.loc[:,'signed_contrasts'] =  (psy_df['contrastRight'] - psy_df['contrastLeft'])
 
-    ##Add choice -1 etc
-    
-    ##TO DELETE###
-    
-    start = time.time()
-    for date in sorted(psy_df['ses'].unique()):
-        for i in range(psy_df.loc[psy_df['ses'] == date,'choice'].shape[0]):
-            psy_df.loc[psy_df['ses'] == date,'choice%s' %str(i+1)] =  psy_df.loc[psy_df['ses'] == date,'choice'].shift(i+1) #no point in 0 shift
-    
-    end = time.time()
-    print(end - start)
-        
-    #######
         
     #Add sex if not present
     if not 'sex' in psy_df:
@@ -116,49 +107,111 @@ def glm_reward(psy_df,  mixed_effects=True):
     
     
     #Remove first 5 trials from each ses
-    for date in sorted(data['ses'].unique()):
-        data  = data.drop(data.index[data['ses'] == date][0:5] ,axis=0)
+    #for date in sorted(data['ses'].unique()):
+    #    data  = data.drop(data.index[data['ses'] == date][0:5] ,axis=0)
         
-    data = data.reset_index()
+    #data = data.reset_index()
     #Drop unnecessary elements
-    data =  data.drop(columns  = ['feedbackType','sex', 'ses','signed_contrasts', 'index'])
+    #data =  data.drop(columns  = ['feedbackType','sex', 'ses','signed_contrasts', 'index'])
     
+    data =  data.dropna()
+    
+    mdata = data.loc[(data['sex'] == 'F')]
+    fdata = data1.loc[(data1['sex'] == 'M')]
     ## construct our model, with contrast as a variable
     
     ##Bayeasian mixed effects #need to change ident and exog_VC to account for mixed effects
-     
+    result, r2  =  load_regression(data)
+    mresult, mr2 = load_regression(mdata)
+    fresult, fr2 = load_regression(fdata)
+    
+    return mresult, fresult, mr2, fr2
+
+def load_regression (data, mixed_effects  = False):
+    
     endog  = pd.DataFrame(data['choice'])
-    exog  = data[[ 'Revidence', 'Levidence', 'rchoice1', 'uchoice1', 'Levidence1',\
+    exog  = data[[ 'Revidence', 'Levidence',\
+            'rchoice1', 'uchoice1', 'Levidence1',\
                   'Revidence1', 'rchoice2', 'uchoice2', 'Levidence2', 'Revidence2',\
                   'rchoice3', 'uchoice3', 'Levidence3', 'Revidence3', 'rchoice4',\
                   'uchoice4', 'Levidence4', 'Revidence4', 'rchoice5', 'uchoice5',\
                   'Levidence5', 'Revidence5']]
-    exog_vc =  np.ones((7508, 1))
-    ident = np.ones(1)
-    model1 = bayes.BinomialBayesMixedGLM(endog, exog, exog_vc, ident)
-    result = model1.fit_map()
-    result.summary()
+    
+    cols = list(exog.columns)
+    
+    #Normalising predictors
+    for col in cols:
+        col_zscore = col + '_zscore'
+        exog[col_zscore] = (exog[col] - exog[col].mean())/exog[col].std(ddof=0)
+    
+    exog  =exog.drop(columns = [ 'Revidence', 'Levidence',\
+            'rchoice1', 'uchoice1', 'Levidence1',\
+                      'Revidence1', 'rchoice2', 'uchoice2', 'Levidence2', 'Revidence2',\
+                      'rchoice3', 'uchoice3', 'Levidence3', 'Revidence3', 'rchoice4',\
+                      'uchoice4', 'Levidence4', 'Revidence4', 'rchoice5', 'uchoice5',\
+                      'Levidence5', 'Revidence5'] )
+        
+    
+    ##Cross validation
     
     if mixed_effects == False :
 
     ##Frequentist wihtout mixed effects
-        start = time.time()
-
-        logit_model = sm.Logit(endog,exog)
+        X_train, X_test, y_train, y_test = train_test_split(exog, np.ravel(endog), test_size=0.3)
+        logit_model = sm.Logit(y_train,X_train)
         result=logit_model.fit()
         print(result.summary2())
 
-      
-        end = time.time()
-        print(end - start)
+        #crossvalidation
         
+        
+        cv = KFold(n_splits=10,  shuffle=False)
+        logreg1 = LogisticRegression()
+        r2  = cross_val_score(logreg1, exog, endog, cv=10)
+        print( 'Accuracy  = ' , r2.mean())
+        print(end - start)
+
+       
     #cross validate  with sklearn 
     #NOTE: Currently cross validating without mixed effects
     
-    X_train, X_test, y_train, y_test = train_test_split(exog, np.ravel(endog), test_size=0.3)
-    logreg = LogisticRegression()
-    logreg.fit(X_train, y_train)
-    accu = logreg.score(X_test, y_test)
-    print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
+        logreg = LogisticRegression()
+        logreg.fit(X_train, y_train)
+        accu = logreg.score(X_test, y_test)
+        print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test, y_test)))
 
-    return result, accu
+    return result, r2
+
+
+""" TODO nmiced effects GLM
+
+     X_train = X_train.drop(columns = 'mouse_name')
+    X_test = X_test.drop(columns = 'mouse_name')
+    y_train = pd.DataFrame(y_train)
+    y_test = pd.DataFrame(y_test)
+    Mixed effects models no done yet
+    y_train = pd.DataFrame(y_train)
+    exog_vc = X_train
+    for i, name in enumerate(sorted(X_train['mouse_name'].unique())):
+        exog_vc.loc[(exog_vc['mouse_name'] == name)] = i
+    ident =  np.ones(exog_vc.shape[1], dtype=np.int)
+    X_train = X_train.drop(columns = 'mouse_name')
+    X_test = X_test.drop(columns = 'mouse_name')
+    exog_vc  = exog_vc.drop(columns = 'mouse_name')
+    exog_vc = pd.DataFrame(exog_vc.iloc[:,1])
+    exog_vc  = exog_vc + 1
+    ident  = np.ones([1])  
+    model1 = bayes.BinomialBayesMixedGLM(y_train, X_train, exog_vc,ident)
+    result = model1.fit_map()
+    result.summary()
+    """"
+    
+
+#####
+#To do model psychometric
+####
+
+
+
+    
+    

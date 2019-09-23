bd30835d22ae88ad273d14e0d004c8b6763cdb2c
Alex Pan <alejandropan@gmail.com>
2019-09-23 11:44:12 -0400

    Update task_settings.py

--- tasks/_iblrig_tasks_biasedLaserWorld/task_settings.py
+++ tasks/_iblrig_tasks_biasedLaserWorld/task_settings.py
@@ -12,7 +12,7 @@ OPEN_CAMERA_VIEW = True  # if RECORD_VIDEO == True OPEN_CAMERA_VIEW is ignored
 # REWARDS
 AUTOMATIC_CALIBRATION = True  # Wether to look for a calibration session and func to define the valve opening time  # noqa
 CALIBRATION_VALUE = 0.067  # calibration value for 3ul of target reward amount (ignored if automatic ON)  # noqa
-REWARD_AMOUNT = 1.5  # (µl) Amount of reward to be delivered upon correct choice each trial (overwitten if adaptive ON)  # noqa
+REWARD_AMOUNT = 1.5  # (Âµl) Amount of reward to be delivered upon correct choice each trial (overwitten if adaptive ON)  # noqa
 REWARD_TYPE = 'Water 10% Sucrose'  # Water, Water 10% Sucrose, Water 15% Sucrose, Water 2% Citric Acid (Guo et al.. PLoS One 2014)  # noqa
 # TASK
 NTRIALS = 2000  # Number of trials for the current session
@@ -22,12 +22,14 @@ BONSAI_EDITOR = False  # Whether to open the visual stim Bonsai editor or not
 REPEAT_ON_ERROR = False
 # STATE TIMERS
 QUIESCENCE_THRESHOLDS = [-2, 2]  # degree
-QUIESCENT_PERIOD = 0.2  # + x, where x~exp(0.35), t ∈ 0.2 <= R <= 0.5
+QUIESCENT_PERIOD = 0.2  # + x, where x~exp(0.35), t â?? 0.2 <= R <= 0.5
 INTERACTIVE_DELAY = 0.0  # (s) how long after stim onset the CL starts
 RESPONSE_WINDOW = 60  # Time to move the wheel after go tone (seconds)
 ITI_CORRECT = 1  # how long the stim should stay visible after CORRECT choice
 ITI_ERROR = 2  # how long the stim should stay visible after ERROR choice
 # VISUAL STIM
+
+HEM_STIM = "B"
 STIM_FREQ = 0.10  # Probably constant - NOT IN USE
 STIM_ANGLE = 0.  # Vertical orientation of Gabor patch - NOT IN USE
 STIM_SIGMA = 7.  # (azimuth_degree) Size of Gabor patch
@@ -43,7 +45,7 @@ BLOCK_LEN_MAX = 100
 # POSITIONS
 STIM_POSITIONS = [-35, 35]  # All possible positions for this session (deg)
 # CONTRASTS
-CONTRAST_SET = [1., 0.25, 0.125, 0.0625, 0.]  # Full contrast set
+CONTRAST_SET = [0.25, 0.125, 0.0625, 0.]  # Full contrast set
 CONTRAST_SET_PROBABILITY_TYPE = 'uniform'  # 'biased' or 'uniform'. Will half the probability of drawing a 0.  # noqa
 # SOUNDS
 SOFT_SOUND = 'xonar'  # Use software sound 'xonar', 'sysdefault' or None for BpodSoundCard  # noqa

b33a0b613e6303e7458331933823b0301641082f
Alejandro Pan Vazquez <alexpan@dynamic-oit-vapornet100-10-9-130-217.princeton.edu>
2022-03-14 16:38:28 -0400

    tasks

--- tasks/_iblrig_tasks_passive_dopamine/task_settings.py
+++
@@ -1,9 +0,0 @@
-﻿#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-# @Author: Niccolò Bonacchi
-# @Date: Tuesday, November 27th 2018, 2:10:03 pm
-"""
-Main settings file for dopamine passive stimulation
-"""
-IBLRIG_FOLDER = "C:\\iblrig"
-IBLRIG_DATA_FOLDER = None  # If None will be ..\\iblrig_data from IBLRIG_FOLDER
